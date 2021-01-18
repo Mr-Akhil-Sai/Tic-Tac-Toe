f60cbcfc3e10eedb099847410162e6d4bbab4715
Mr.Ak <akhilsaivaraganti@gmail.com>
2021-01-18 12:35:50 +0530

    need to work on the logic

---
+++ app.js
@@ -0,0 +1,20 @@
+let button1 = document.querySelector(".button-1");
+let button2 = document.querySelector(".button-2");
+let button3 = document.querySelector(".button-3");
+let button4 = document.querySelector(".button-4");
+let button5 = document.querySelector(".button-5");
+let button6 = document.querySelector(".button-6");
+let button7 = document.querySelector(".button-7");
+let button8 = document.querySelector(".button-8");
+let button9 = document.querySelector(".button-9");
+
+
+let player1;
+let player2;
+
+
+
+
+button1.addEventListener("click", function changeName(){
+    button1.innerHTML= "X";
+});
