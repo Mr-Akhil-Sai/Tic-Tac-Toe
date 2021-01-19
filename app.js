187c11cecd283138af06172f1d9956142f0f0f9a
Mr.Ak <akhilsaivaraganti@gmail.com>
2021-01-19 12:16:37 +0530

    Done with appending X and O

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
+let flag = 1;
 
 document.querySelectorAll(".block").forEach((el)=>
 { 
-    el.addEventListener('click',()=>{console.log(el)})
+    el.addEventListener('click',()=>
+    {
+       if (flag === 1) {
+           el.innerHTML = "X";
+           el.style.pointerEvents = "none";
+           flag = 0;
+       }
+        else{
+            el.innerHTML = "O";
+            el.disabled = true;
+            flag = 1;
+        }
+    })
 });
