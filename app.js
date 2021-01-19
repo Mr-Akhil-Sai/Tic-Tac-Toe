295b41caef7453e74399c58a5ad40becf9042601
Mr.Ak <akhilsaivaraganti@gmail.com>
2021-01-19 13:16:40 +0530

    need to work on winning logic

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
 let flag = 1;
 
-document.querySelectorAll(".block").forEach((el)=>
+document.querySelectorAll(".block").forEach((el, index)=>
 { 
+    console.log(index);
     el.addEventListener('click',()=>
     {
        if (flag === 1) {
            el.innerHTML = "X";
            el.style.pointerEvents = "none";
            flag = 0;
+           winning(el.innerHTML ,index);
        }
         else{
             el.innerHTML = "O";
-            el.disabled = true;
-            flag = 1;
+            el.style.pointerEvents = "none"; 
+            flag = 1;   
         }
     })
 });
+
+//winning logic for X
+let update = document.querySelector("#update");
+
+function winning(a, b){
+    console.log(a)
+    console.log(b);
+    if(a[b] == "X" && a[b] == "X" && a[b] == "X"){
+        console.log(a[b]);
+        update.innerHTML = "player X won";
+    }
+}
+
+
