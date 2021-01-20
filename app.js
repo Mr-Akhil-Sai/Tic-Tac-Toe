edba512241c8b9e5fcc56f759b23edc1af7bbc60
Mr.Ak <akhilsaivaraganti@gmail.com>
2021-01-20 12:56:56 +0530

    changed bg color to the blocks

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ function disableBlockEvents(section){
     section.style.pointerEvents = "none";
 }
 
-//function for adding bgcolor to the winning blocks of x
-function bgcolor(a,b,c){
-    block[a].style.backgroundColor = "red";
-    block[b].style.backgroundColor = "red";
-    block[c].style.backgroundColor = "red";
+
+//function for font size and font weight
+function font(el){
+    el.style.fontSize = "1.7em";
+    el.style.fontWeight = "900";
 }
 
 
@@ -28,11 +28,17 @@ block.forEach((el)=>
         el.addEventListener('click',()=>
     {
        if (flag === 1) {
+           el.style.backgroundColor = "green";
+           el.style.color = "whitesmoke";
+           font(el);
            el.innerHTML = "X";
            playerTurn.innerHTML = 'player "O" turn';
            flag = 0;
         }
         else{
+            el.style.backgroundColor = "goldenrod";
+            el.style.color = "whitesmoke";
+            font(el);
             el.innerHTML = "O";
             playerTurn.innerHTML = 'player "X" turn';
             flag = 1;   
@@ -51,56 +57,48 @@ let indexes = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 function winning(){
     if(block[0].innerHTML == "X" && block[1].innerHTML == "X" && block[2].innerHTML == "X"){
         update.style.color = "green";
-        bgcolor(0,1,2);
         update.innerHTML = 'Player "X" won'
         playerTurn.innerHTML = '';
         disableBlockEvents(section);
     }
     else if(block[0].innerHTML == "X" && block[4].innerHTML == "X" && block[8].innerHTML == "X"){
         update.style.color = "green";
-        bgcolor(0,4,8);
         update.innerHTML = 'Player "X" won'
         playerTurn.innerHTML = '';
         disableBlockEvents(section);
     }
     else if(block[0].innerHTML == "X" && block[3].innerHTML == "X" && block[6].innerHTML == "X"){
         update.style.color = "green";
-        bgcolor(0,3,6);
         update.innerHTML = 'Player "X" won'
         playerTurn.innerHTML = '';
         disableBlockEvents(section);
     }
     else if(block[2].innerHTML == "X" && block[4].innerHTML == "X" && block[6].innerHTML == "X"){
         update.style.color = "green";
-        bgcolor(2,4,6);
         update.innerHTML = 'Player "X" won'
         playerTurn.innerHTML = '';
         disableBlockEvents(section);
     }
     else if(block[3].innerHTML == "X" && block[4].innerHTML == "X" && block[5].innerHTML == "X"){
         update.style.color = "green";
-        bgcolor(3,4,5);
         update.innerHTML = 'Player "X" won'
         playerTurn.innerHTML = '';
         disableBlockEvents(section);
     }
     else if(block[6].innerHTML == "X" && block[7].innerHTML == "X" && block[8].innerHTML == "X"){
         update.style.color = "green";
-        bgcolor(6,7,8);
         update.innerHTML = 'Player "X" won'
         playerTurn.innerHTML = '';
         disableBlockEvents(section);
     }
     else if(block[1].innerHTML == "X" && block[4].innerHTML == "X" && block[7].innerHTML == "X"){
         update.style.color = "green";
-        bgcolor(1,4,7);
         update.innerHTML = 'Player "X" won'
         playerTurn.innerHTML = '';
         disableBlockEvents(section);
     }
     else if(block[2].innerHTML == "X" && block[5].innerHTML == "X" && block[8].innerHTML == "X"){
         update.style.color = "green";
-        bgcolor(2,5,8);
         update.innerHTML = 'Player "X" won'
         playerTurn.innerHTML = '';
         disableBlockEvents(section);
@@ -110,56 +108,48 @@ function winning(){
     //winning logic for 'O'
     else if(block[0].innerHTML == "O" && block[1].innerHTML == "O" && block[2].innerHTML == "O"){
         update.style.color = "goldenrod";
-        bgcolor(0,1,2);
         update.innerHTML = 'Player "O" won' 
         playerTurn.innerHTML = '';
         disableBlockEvents(section);
     }
     else if(block[0].innerHTML == "O" && block[4].innerHTML == "O" && block[8].innerHTML == "O"){
         update.style.color = "goldenrod";
-        bgcolor(0,4,8);
         update.innerHTML = 'Player "O" won'
         playerTurn.innerHTML = '';
         disableBlockEvents(section);
     }
     else if(block[0].innerHTML == "O" && block[3].innerHTML == "O" && block[6].innerHTML == "O"){
         update.style.color = "goldenrod";
-        bgcolor(0,3,6);
         update.innerHTML = 'Player "O" won'
         playerTurn.innerHTML = '';
         disableBlockEvents(section);
     }
     else if(block[2].innerHTML == "O" && block[4].innerHTML == "O" && block[6].innerHTML == "O"){
         update.style.color = "goldenrod";
-        bgcolor(2,4,6);
         update.innerHTML = 'Player "O" won'
         playerTurn.innerHTML = '';
         disableBlockEvents(section);
     }
     else if(block[3].innerHTML == "O" && block[4].innerHTML == "O" && block[5].innerHTML == "O"){
         update.style.color = "goldenrod";
-        bgcolor(3,4,5);
         update.innerHTML = 'Player "O" won'
         playerTurn.innerHTML = '';
         disableBlockEvents(section);
     }
     else if(block[6].innerHTML == "O" && block[7].innerHTML == "O" && block[8].innerHTML == "O"){
         update.style.color = "goldenrod";
-        bgcolor(6,7,8);
         update.innerHTML = 'Player "O" won'
         playerTurn.innerHTML = '';
         disableBlockEvents(section);
     }
     else if(block[1].innerHTML == "O" && block[4].innerHTML == "O" && block[7].innerHTML == "O"){
         update.style.color = "goldenrod";
-        bgcolor(1,4,7);
         update.innerHTML = 'Player "O" won'
         playerTurn.innerHTML = '';
         disableBlockEvents(section);
     }
     else if(block[2].innerHTML == "O" && block[5].innerHTML == "O" && block[8].innerHTML == "O"){
         update.style.color = "goldenrod";
-        bgcolor(2,4,8);
         update.innerHTML = 'Player "O" won'
         playerTurn.innerHTML = '';
         disableBlockEvents(section);
